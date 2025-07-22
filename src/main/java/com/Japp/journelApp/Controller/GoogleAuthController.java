7df6486dfe70b05b4a37ae3797356db7b71fde71
rahul54726 <vermarahul11034@gmail.com>
2025-07-22 17:50:30 +0530

    Adding OAuth2

---
+++ src/main/java/com/Japp/journelApp/Controller/GoogleAuthController.java
@@ -0,0 +1,84 @@
+package com.Japp.journelApp.Controller;
+
+import com.Japp.journelApp.Entity.User;
+import com.Japp.journelApp.repository.UserRepo;
+import com.Japp.journelApp.service.UserDetailsServiceImpl;
+import io.jsonwebtoken.security.Password;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+@Slf4j
+@RestController
+@RequestMapping("/auth/google")
+public class GoogleAuthController {
+    @Value("${spring.security.oauth2.client.registration.google.client-id}")
+    private String clientId;
+    @Value("${spring.security.oauth2.client.registration.google.client-secret}")
+    private String clientSecret;
+    @Autowired
+    private RestTemplate restTemplate;
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @Autowired
+    private UserRepo userRepo;
+    @GetMapping("/callback")
+    public ResponseEntity<?> handleGoogleCallback(@RequestParam String code){
+        try {
+            // 1. Exchange auth code for tokens
+            String tokenEndPoint = "https://oauth2.googleapis.com/token";
+            HashMap<String ,String> params = new HashMap<>();
+            params.put("code",code);
+            params.put("client_id",clientId);
+            params.put("client_secret", clientSecret);
+            params.put("redirect_uri", "https://developers.google.com/oauthplayground");
+            params.put("grant_type ", "authorization_code" );
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+            HttpEntity<Map<String,String>> request = new HttpEntity<>(params,headers);
+            ResponseEntity<Map> tokenResponse = restTemplate.postForEntity(tokenEndPoint, request, Map.class);
+            String idToken = (String)  tokenResponse.getBody().get("id_token");
+            String userInfoUrl = "https://oauth2.googleapis.com/tokeninfo?id_token=" + idToken;
+            ResponseEntity<Map> userInfoResponse = restTemplate.getForEntity(userInfoUrl, Map.class);
+            if(userInfoResponse.getStatusCode() == HttpStatus.OK){
+                Map<String ,Object> userInfo = userInfoResponse.getBody();
+                String email = (String) userInfo.get("email");
+                UserDetails userDetails = userDetailsService.loadUserByUsername(email);
+                if(userDetails == null) {
+                    User user = new User();
+                    user.setEmail(email);
+                    user.setUserName(email);
+                    user.setPassword(passwordEncoder.encode(UUID.randomUUID().toString()));
+                    user.setRoles(Arrays.asList("USER"));
+                    userRepo.save(user);
+                    userDetails =  userDetailsService.loadUserByUsername(email);
+                }
+                UsernamePasswordAuthenticationToken authentication =
+                        new UsernamePasswordAuthenticationToken(userDetails,null,userDetails.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+                return ResponseEntity.status(HttpStatus.OK).build();
+            }
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }catch (Exception e){
+            log.error("Something worng with OAuth2",e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+}
--- src/main/java/com/Japp/journelApp/Entity/User.java
+++ src/main/java/com/Japp/journelApp/Entity/User.java
@@ -1,9 +1,6 @@
 package com.Japp.journelApp.Entity;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NonNull;
-import lombok.Setter;
+import lombok.*;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
@@ -14,13 +11,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Document(collection ="users")
+
 @Getter
 @Setter
 @Builder
 // below two annotations are of Lombok Project used to generate Getters And Setters at the time of Compilations
 // there are many of them to use
 public class User {
-
     @Id
     private ObjectId id;
     //isko uniqye banane se ye indexing unique nahi hogi numko application properties me jake mention karana pdega ki
@@ -36,4 +33,7 @@ public class User {
     private List<JournalEntry> JournalEntries = new ArrayList<>();
     private List<String> roles;
 
+    public User() {
+
+    }
 }
